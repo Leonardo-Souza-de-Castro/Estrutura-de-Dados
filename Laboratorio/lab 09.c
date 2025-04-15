4438a9e6e7f50f98ce590a2863a3176256023c52
LEONARDO SOUZA DE CASTRO <uniflcastro@feinet.fei.edu.br>
2025-04-14 21:39:22 -0300

    lab 09 finalizado

--- Laboratorio/lab 09.c
+++ Laboratorio/lab 09.c
@@ -3,67 +3,124 @@
 
 #define tam_hash 11
 
-typedef struct Celula{
-  int valor;
-  struct Celula* proximo;
-}Celula;
+typedef struct Celula
+{
+    int valor;
+    struct Celula *proximo;
+} Celula;
 
-typedef struct {
-  Celula* inicio;
-}Lista;
+typedef struct
+{
+    Celula *inicio;
+} Lista;
 
-typedef struct {
-  Lista* table[tam_hash];
-}Hash;
+typedef struct
+{
+    Lista *table[tam_hash];
+} Hash;
 
-Hash* start_hash(){
-  Hash* hash = malloc(sizeof(Hash));
-  for(int i = 0; i < tam_hash; i++){
-    Lista* lista = malloc(sizeof(Lista));
-    lista->inicio = NULL;
-    hash->table[i] = lista;
-  }
-  return hash;
-}
-
-void inserir_hash(Hash* hash, int valor){
-  // implementar
-}
-
-void remover_hash(Hash* hash, int valor){
-  // implementar
-}
-
-void imprimir(Hash* hash){
-  printf("---------------------\n");
-  for(int i = 0; i < tam_hash; i++){
-    Celula* atual = hash->table[i]->inicio;
-    printf("%d -> ", i);
-    while(atual != NULL){
-      printf("%d ", atual->valor);
-      atual = atual->proximo;
+Hash *start_hash()
+{
+    Hash *hash = malloc(sizeof(Hash));
+    for (int i = 0; i < tam_hash; i++)
+    {
+        Lista *lista = malloc(sizeof(Lista));
+        lista->inicio = NULL;
+        hash->table[i] = lista;
     }
-    printf("\n");
-  }
-  printf("---------------------\n");
+    return hash;
 }
 
-int main(void) {
-  Hash* hash = start_hash();
-  int valor;
-  while(1){
-    scanf("%d", &valor);
-    if(valor == 0)
-      break;
-    inserir_hash(hash, valor);
-  }
-  imprimir(hash);
-  while(1){
-    scanf("%d", &valor);
-    if(valor == 0)
-      break;
-    remover_hash(hash, valor);
-  }
-  imprimir(hash);
-  return 0;
+Celula *criar_celula(int valor)
+{
+    Celula *nova = malloc(sizeof(Celula));
+    nova->proximo = NULL;
+    nova->valor = valor;
+    return nova;
+}
+
+void inserir_hash(Hash *hash, int valor)
+{
+    int pos = valor % tam_hash;
+
+    if (pos >= tam_hash)
+    {
+        return;
+    }
+    else
+    {
+
+        Celula *nova = criar_celula(valor);
+        nova->proximo = hash->table[pos]->inicio;
+        hash->table[pos]->inicio = nova;
+    }
+}
+
+void remover_hash(Hash *hash, int valor)
+{
+    int pos = valor % tam_hash;
+    Celula *atual = hash->table[pos]->inicio;
+    Celula *anterior = NULL;
+
+    while (atual != NULL && atual->valor != valor)
+    {
+        anterior = atual;
+        atual = atual->proximo;
+    }
+
+    if (atual == NULL)
+    {
+        return;
+    }
+
+    if (anterior == NULL)
+    {
+        hash->table[pos]->inicio = atual->proximo;
+        free(atual);
+    }
+    else
+    {
+        anterior->proximo = atual->proximo;
+        free(atual);
+    }
+}
+
+void imprimir(Hash *hash)
+{
+    printf("---------------------\n");
+    for (int i = 0; i < tam_hash; i++)
+    {
+        Celula *atual = hash->table[i]->inicio;
+        printf("%d -> ", i);
+        while (atual != NULL)
+        {
+            printf("%d ", atual->valor);
+            atual = atual->proximo;
+        }
+        printf("\n");
+    }
+    printf("---------------------\n");
+}
+
+int main(void)
+{
+    Hash *hash = start_hash();
+    int valor;
+    while (1)
+    {
+        scanf("%d", &valor);
+        if (valor == 0)
+            break;
+        inserir_hash(hash, valor);
+    }
+    imprimir(hash);
+    while (1)
+    {
+        scanf("%d", &valor);
+        if (valor == 0)
+            break;
+        remover_hash(hash, valor);
+    }
+    imprimir(hash);
+    return 0;
 }
